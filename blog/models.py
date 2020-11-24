18765ddd121f8fa09d45fcd7c53fb56cb5a22b29
Gaston Gouveia <gastongouveia@gmail.com>
2020-11-24 02:44:51 -0300

    implementação do botão para deletar posts

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    situacao = models.CharField(max_length=7, default='ativo')
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -37,3 +37,9 @@ def post_edit(request, pk):
     else:
         form = Postform(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.situacao = 'inativo'
+    post.save()
+    return redirect('post_list')
